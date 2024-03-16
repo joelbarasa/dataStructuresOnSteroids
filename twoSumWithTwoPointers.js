0aeff2381bf4fa1d1fa019be509ff9d3b3c00426
Joel Barasa <barasajoel@gmail.com>
2024-03-16 16:50:11 +0300

    Create twoSumWithTwoPointers.js
    https://leetcode.com/problems/two-sum/

---
+++ twoSumWithTwoPointers.js
@@ -0,0 +1,22 @@
+//Two Sum with pointers for sorted array
+let nums = [2, 8, 7, 11, 15],
+  target = 9;
+
+res = twoSumWithSortedArray(nums, target);
+console.log(res);
+
+function twoSumWithSortedArray(nums, target) {
+  let i = 0,
+    j = nums.length - 1;
+  while (i < j) {
+    let sum = nums[i] + nums[j];
+    if (sum < target) {
+      i++;
+    } else if (sum > target) {
+      j--;
+    } else {
+        // console.log(i + 1, j  + 1);
+      return [i, j];
+    }
+  }
+}
