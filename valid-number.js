2cf6277f7dc5b58753335bda4b1740c899d013fe
Joel Barasa <barasajoel@gmail.com>
2024-03-27 06:07:18 +0300

    Create valid-number.js
    https://leetcode.com/problems/valid-number/

---
+++ valid-number.js
@@ -0,0 +1,39 @@
+var isNumber = function (s) {
+    let isValid = true;
+    for (let i = 0; i < s.length; i++) {
+        let previous = i - 1;
+        let next = i + 1;
+
+        if (i < s.length && isNaN(s[i]) === true && (s.charAt(i) !== 'e' || i < s.length && s.charAt(i) !== 'E') && (s[i] !== '+' || s[i] !== '-')) {
+            isValid = false;
+            return false;
+
+        }
+        if (i < s.length && (s.charAt(i) === 'e' || i < s.length && s.charAt(i) === 'E')) {
+            if (previous < 0 || next > s.length) {
+                return false;
+            }
+            else {
+                if (isNaN(s[previous]) === true && isNaN(s[next]) === true) {
+                    return false;
+                }
+            }
+        }
+
+        if (s[i] === '.') {
+            if (previous < 0 || next > s.length) {
+                return false;
+            }
+            else {
+                // if (isNaN(s[previous]) === true && isNaN(s[next]) === true) {
+                //     return false;
+                // }
+                if (isNaN(s[next]) === true) {
+                    return false;
+                }
+            }
+        }
+
+    }
+    return isValid;
+};
