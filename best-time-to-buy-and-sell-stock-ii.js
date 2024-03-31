c1e3723f604932b37d986ff0f7192fae10d9e4fe
Joel Barasa <barasajoel@gmail.com>
2024-03-31 21:09:31 +0300

    Create best-time-to-buy-and-sell-stock-ii.js
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/

---
+++ best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+
+    if (!Array.isArray(prices) || prices.length < 2) {
+        return 0;
+    }
+    else {
+        let profit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+};
