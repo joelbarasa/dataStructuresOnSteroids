ac91766ab657b1b7b640613f34a92853fab96980
Joel Barasa <barasajoel@gmail.com>
2024-04-10 20:59:11 +0300

    Update find-all-duplicates-in-an-array.js
    https://leetcode.com/problems/find-all-duplicates-in-an-array/

--- find-all-duplicates-in-an-array.js
+++ find-all-duplicates-in-an-array.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// Option 1
 var findDuplicates = function (nums) {
     // nums = [1,1,2]; nums = [1];
     let set = new Set();
@@ -13,3 +15,26 @@ var findDuplicates = function (nums) {
     return Array.from(set);
 };
 
+
+//Option 2
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {    
+    let map = new Map();
+    let res = new Array();
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) {
+            map.set(nums[i], 1);
+        }
+        else {
+            map.set(nums[i], map.get(nums[i]) + 1);
+        }
+    }
+
+    for (let [key, value] of map) {
+        if (value === 2) res.push(key);
+    }
+    return res;
+};
