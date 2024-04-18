7c82e4182201d13f309e64a527c7c550a1aaa611
Joel Barasa <barasajoel@gmail.com>
2024-04-18 20:48:26 +0300

    Update longest-substring-without-repeating-characters.js

--- longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters.js
@@ -2,6 +2,10 @@
  * @param {string} s
  * @return {number}
  */
+
+
+// Option 1
+
 var lengthOfLongestSubstring = function (s) {
     // s= " ";
     let longestSubstring = 0;
@@ -24,4 +28,24 @@ var lengthOfLongestSubstring = function (s) {
     }
     console.log(longestSubstring);
     return longestSubstring;
+};
+
+
+// Option 2
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let start = 0;
+    let max = 0;
+    let map = new Map();
+    for (let end = 0; end < s.length; end++) {
+        if (map.has(s[end])) {
+            start = Math.max(start, map.get(s[end]) + 1);
+        }
+        map.set(s[end], end);
+        max = Math.max(max, end - start + 1);
+    }
+    return max;
 };
