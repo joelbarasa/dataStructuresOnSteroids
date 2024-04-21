9c3bd3e5b8727189ed2cfaa9da08b3db123b847e
Joel Barasa <barasajoel@gmail.com>
2024-04-21 17:00:25 +0300

    Update remove-duplicates-from-sorted-array.js

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -9,3 +9,35 @@ var removeDuplicates = function (nums) {
         }
     }
 };
+
+
+var removeDuplicates = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === nums[i+1]){
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+};
+
+
+
+
+
+function removeDuplicates(nums) {
+  //Shift elements to the left whenever duplicates are encountered
+
+  //One pointer for iterating
+  let i = 1;
+
+  //One pointer for placing next non duplicate
+  let nextNoneDuplicate = 1;
+  while (i < nums.length) {
+    if (nums[nextNoneDuplicate - 1] !== nums[i]) {
+      nums[nextNoneDuplicate] = nums[i];
+      nextNoneDuplicate++;
+    }
+    i++;
+  }
+  return nextNoneDuplicate;
+}
