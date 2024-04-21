3cac3ee62c8dd5e442c6bc4f7a1a5946922c55a6
Joel Barasa <barasajoel@gmail.com>
2024-04-21 17:41:29 +0300

    Create squares-of-a-sorted-array.js
    https://leetcode.com/problems/squares-of-a-sorted-array/

---
+++ squares-of-a-sorted-array.js
@@ -0,0 +1,6 @@
+var sortedSquares = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = Math.pow(nums[i], 2);
+    }
+    return nums.sort((a, b) => (a < b) ? -1 : 0);
+};
