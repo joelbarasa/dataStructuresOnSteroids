82a0129467fa43ee9ef334cb3882111898cf477a
Joel Barasa <barasajoel@gmail.com>
2024-04-22 17:22:41 +0300

    Update linked-list-cycle.js

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -27,3 +27,33 @@ var hasCycle = function(head) {
     }
 };
 
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    // Use two pointer approach - hare and tortoise algorithm
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        // Found a cycle
+        if (slow === fast) {
+            return true
+        }
+
+    }
+    return false;
+};
+
