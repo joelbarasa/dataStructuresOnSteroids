cdfa8a12633cb2801197d42b7059ef2202d95ef2
Joel Barasa <barasajoel@gmail.com>
2024-05-19 10:45:11 +0300

    Create single-number-iii.js
    https://leetcode.com/problems/single-number-iii/

---
+++ single-number-iii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function (nums) {
+
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            map.delete(nums[i]);
+        }
+        else {
+            map.set(nums[i], 1);
+        }
+    }
+    return Array.from(map.keys());
+};
