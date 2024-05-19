fd45def94eacfa6c73b392a70d799fd4b75c5f4b
Joel Barasa <barasajoel@gmail.com>
2024-05-19 09:44:36 +0300

    Create single-number.js

---
+++ single-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], map.get(nums[i]) + 1 || 1);
+    }
+    for (let [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
+
+};
