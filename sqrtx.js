25b16b15bc74ee08fe4dfc9a36f985386ede9b69
Joel Barasa <barasajoel@gmail.com>
2024-06-01 11:48:08 +0300

    Create sqrtx.js
    https://leetcode.com/problems/sqrtx/

---
+++ sqrtx.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    let left = 1;
+    let right = x;
+    let sqrt = -1;
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if ((mid * mid) === x) {
+            return mid;
+        }
+        else if ((mid * mid) > x) {
+            sqrt = mid;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return sqrt - 1;
+};
+
+
+
