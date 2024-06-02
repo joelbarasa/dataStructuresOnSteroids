234688f086610c99d982bb13f3f2a200d71ac97b
Joel Barasa <barasajoel@gmail.com>
2024-06-02 21:19:12 +0300

    Update move-zeroes.js

--- move-zeroes.js
+++ move-zeroes.js
@@ -18,12 +18,12 @@ var moveZeroes = function (nums) {
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            let zeroValue = nums.splice(i, 1);
-            nums.push(zeroValue);
-            i--;
+ // Using two pointers
+    let slow = 0;
+    for (let fast = 0; fast < nums.length; fast++) {
+        if (nums[fast] !== 0) {
+            [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+            slow++;
         }
     }
-    return nums;
 };
