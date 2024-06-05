83977a93e638743b343f0fbadbcf0338b5875250
Joel Barasa <barasajoel@gmail.com>
2024-06-05 03:22:53 +0300

    Update maximum-average-subarray-i.js

--- maximum-average-subarray-i.js
+++ maximum-average-subarray-i.js
@@ -20,3 +20,27 @@ var findMaxAverage = function (nums, k) {
     return max / k;
 };
 
+
+//THE BEST SOLUTION IS HERE...SLIDING WINDOW!
+var findMaxAverage = function (nums, k) {
+    let averageSum = new Array();
+    let windowStart = 0;
+    let sum = 0;
+    for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        sum += nums[windowEnd];
+
+        if(windowEnd >= k -1){
+            averageSum.push(sum/k);
+            sum = sum - nums[windowStart];
+            windowStart++;
+        }
+
+    }
+    return Math.max(...averageSum);
+};
+
+    //Use sliding window approach
+    //sum eleemnts until size k
+    //push the sum to average  array
+    // slide the window foraward and repeat
+
