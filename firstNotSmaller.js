000bda201c7538f9f9c032487dba2b511ef99e2a
Joel Barasa <barasajoel@gmail.com>
2024-06-07 22:38:04 +0300

    Update firstNotSmaller.js
    https://algo.monster/problems/binary_search_first_element_not_smaller_than_target

--- firstNotSmaller.js
+++ firstNotSmaller.js
@@ -4,22 +4,29 @@ target = 8;
 let compute = firstNotSmaller(arr, target);
 console.log(compute);
 function firstNotSmaller(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
-  let boundary_index = -1;
-  while (left <= right) {
-    let mid = left + Math.floor((right - left) / 2);
-    if (arr[mid] >= target) {
-      boundary_index = mid - 1;
-      right = mid - 1;
-    } else {
-      left = mid + 1;
+    // WRITE YOUR BRILLIANT CODE HERE
+
+//     for(let index = 0; index < arr.length; index++){
+//         if(arr[index] >= target) {
+//             return index;
+//         }
+//     }
+//     return arr.length;
+    let left = 0;
+    let right = arr.length -1;
+    let position =-1;
+    while(left <= right){
+        let mid = left + Math.floor((right-left)/2);
+        if(arr[mid] >= target){
+            position = mid;
+            right = mid - 1;
+        }
+        else{
+            left = mid +1;
+        }          
     }
-  }
-  return boundary_index;
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (arr[i] > target) {
-  //       return i - 1;
-  //     }
-  //   }
+    return position;
 }
+
+
+
