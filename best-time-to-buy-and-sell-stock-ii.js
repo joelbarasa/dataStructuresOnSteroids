aff0bc596c099ffd7dbd7cd5c251cc11a81fc34a
Joel Barasa <barasajoel@gmail.com>
2024-06-07 15:35:34 +0300

    Update best-time-to-buy-and-sell-stock-ii.js

--- best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii.js
@@ -1,26 +1,3 @@
-/**
- * @param {number[]} prices
- * @return {number}
- */
-var maxProfit = function (prices) {
-    if (!Array.isArray(prices) || prices.length < 2) {
-        return 0;
-    }
-    else {
-        let minimumIndex = 0;
-        let maximumDeviation = 0;
-        for (let currentIndex = 1; currentIndex < prices.length; currentIndex++) {
-            if (prices[currentIndex] < prices[minimumIndex]) {
-                minimumIndex = currentIndex;
-            }
-
-            maximumDeviation = Math.max(maximumDeviation, prices[currentIndex] - prices[minimumIndex]);
-        }
-        return maximumDeviation;
-    }
-    // return 0;
-};
-
 
 /**
  * @param {number[]} prices
